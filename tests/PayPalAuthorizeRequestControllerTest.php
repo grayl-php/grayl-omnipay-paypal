1f72ddb641106c023a7fac64995412d72b0c6e1b
Grogwood LLC <dev@grogwood.com>
2020-12-04 21:12:57 -0800

    Tests updated to work without orders

--- tests/PayPalAuthorizeRequestControllerTest.php
+++ tests/PayPalAuthorizeRequestControllerTest.php
@@ -6,10 +6,7 @@
    use Grayl\Omnipay\PayPal\Controller\PayPalAuthorizeRequestController;
    use Grayl\Omnipay\PayPal\Controller\PayPalAuthorizeResponseController;
    use Grayl\Omnipay\PayPal\Entity\PayPalGatewayData;
-   use Grayl\Omnipay\PayPal\Helper\PayPalOrderHelper;
    use Grayl\Omnipay\PayPal\PayPalPorter;
-   use Grayl\Store\Order\Controller\OrderController;
-   use Grayl\Store\Order\OrderPorter;
    use PHPUnit\Framework\TestCase;
 
    /**
@@ -55,72 +52,36 @@
 
 
       /**
-       * Creates a test order object with good data
-       * TODO: Change this test data
-       *
-       * @return OrderController
-       * @throws \Exception
-       */
-      public function testCreateOrderController (): OrderController
-      {
-
-         // Create the test object
-         $order = OrderPorter::getInstance()
-                             ->newOrderController();
-
-         // Check the type of object returned
-         $this->assertInstanceOf( OrderController::class,
-                                  $order );
-
-         // Basic order data
-         $data = $order->getOrderData();
-         $data->setAmount( 110.00 );
-         $data->setDescription( 'PayPal test order' );
-
-         // Items
-         $order->putOrderItem( OrderPorter::getInstance()
-                                          ->newOrderItem( $order->getOrderID(),
-                                                          'item1',
-                                                          'Tester Item',
-                                                          '2',
-                                                          13.22 ) );
-         $order->putOrderItem( OrderPorter::getInstance()
-                                          ->newOrderItem( $order->getOrderID(),
-                                                          'item2',
-                                                          'Tester Item 2',
-                                                          '1',
-                                                          8.27 ) );
-
-         // Save the order
-         $order->saveOrder();
-
-         // Return the object
-         return $order;
-      }
-
-
-      /**
        * Tests the creation of a PayPalAuthorizeRequestController object
        *
-       * @param OrderController $order_controller A configured OrderController with order information
-       *
-       * @depends testCreateOrderController
        * @return PayPalAuthorizeRequestController
        * @throws \Exception
        */
-      public function testCreatePayPalAuthorizeRequestController ( OrderController $order_controller ): PayPalAuthorizeRequestController
+      public function testCreatePayPalAuthorizeRequestController (): PayPalAuthorizeRequestController
       {
 
          // Create the object
-         $request = PayPalOrderHelper::getInstance()
-                                     ->newPayPalAuthorizeRequestControllerFromOrder( $order_controller );
+         $request = PayPalPorter::getInstance()
+                                ->newPayPalAuthorizeRequestController( 'test-' . time(),
+                                                                       65.00,
+                                                                       'USD' );
+
+         // Add two items
+         $request->getRequestData()
+                 ->putItem( 'nice-item',
+                            2,
+                            15.00 );
+         $request->getRequestData()
+                 ->putItem( 'test-item',
+                            1,
+                            35.00 );
 
          // Check the type of object returned
          $this->assertInstanceOf( PayPalAuthorizeRequestController::class,
                                   $request );
 
          // Check the total
-         $this->assertEquals( 34.71,
+         $this->assertEquals( 65.00,
                               $request->getRequestData()
                                       ->getAmount() );
 
@@ -134,7 +95,7 @@
        *
        * @param PayPalAuthorizeRequestController $request A configured PayPalAuthorizeRequestController to send
        *
-       * @depends      testCreatePayPalAuthorizeRequestController
+       * @depends testCreatePayPalAuthorizeRequestController
        * @return PayPalAuthorizeResponseController
        * @throws \Exception
        */
@@ -170,50 +131,12 @@
          $this->assertTrue( $response->isRedirect() );
          $this->assertNotNull( $response->getRedirectURL() );
          $this->assertNotNull( $response->getAmount() );
-      }
 
-
-      /**
-       * Creates an OrderPayment object from a PayPalAuthorizeResponseController and checks it for errors
-       *
-       * @param OrderController                   $order_controller A configured OrderController with order information
-       * @param PayPalAuthorizeResponseController $response         A PayPalAuthorizeResponseController returned from the gateway
-       *
-       * @depends      testCreateOrderController
-       * @depends      testSendPayPalAuthorizeRequestController
-       * @throws \Exception
-       */
-      public function testCreateOrderPaymentFromPayPalAuthorizeResponseController ( OrderController $order_controller,
-                                                                                    PayPalAuthorizeResponseController $response ): void
-      {
-
-         // Create a new OrderPayment record from the authorize response
-         PayPalOrderHelper::getInstance()
-                          ->newOrderPaymentFromOmnipayResponseController( $response,
-                                                                          $order_controller,
-                                                                          null );
-
-         // Make sure the order is paid
-         $this->assertFalse( $order_controller->isOrderPaid() );
-
-         // Grab the created payment
-         $payment = $order_controller->getOrderPayment();
-
-         // Test the data
-         $this->assertTrue( $payment->isSuccessful() );
-         $this->assertNotNull( $payment->getReferenceID() );
-         $this->assertEquals( $response->getReferenceID(),
-                              $payment->getReferenceID() );
-         $this->assertEquals( 'redirect',
-                              $payment->getAction() );
-         $this->assertEquals( 'paypal',
-                              $payment->getProcessor() );
-         $this->assertEquals( $response->getAmount(),
-                              $payment->getAmount() );
+         echo $response->getRedirectURL();
       }
 
       // After the request with the redirect URL is approved, this gateway cannot be tested in PHPUnit any further
-      // When the user returns from Paypal's offsite payment, a Complete Purchase and Capture requests must then be completed
+      // When the user returns from Paypal's offsite payment, a 1. Complete Purchase request and a 2. Capture request must then be performed
       // Complete PayPal process tests are located in /tests to be run in browser
 
    }
